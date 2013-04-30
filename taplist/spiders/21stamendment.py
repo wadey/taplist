9165db04a0eabe95a632ffac32b9effb4b867d8d
Wade Simmons <wade@wades.im>
2013-04-30 14:59:01 -0700

    fix 21st

--- taplist/spiders/21stamendment.py
+++ taplist/spiders/21stamendment.py
@@ -8,20 +8,18 @@ from taplist.items import TaplistItem
 class t21stamendmentSpider(CrawlSpider):
     name = '21stamendment'
     allowed_domains = ['http://21st-amendment.com/']
-    start_urls = ['http://21st-amendment.com/restaurant/ontap']
+    start_urls = ['http://21st-amendment.com/the-restaurant/']
 
     def parse(self, response):
         hxs = HtmlXPathSelector(response)
-        beers = hxs.select("//div[contains(@class,'fullBeerSlot')]")
+        beers = hxs.select("//section[@id='beers-on-tap']/dl/dt")
         items = []
         for beer in beers:
             i = TaplistItem()
-            name = ' '.join(beer.select('img/@alt').extract())
-            if not name:
-                name = ' '.join(beer.select("div[contains(@class,'imgSlug')]/text()").extract())
-            desc = ' '.join(beer.select('span/text()').extract())
-
-            i['name'] = re.sub(r' +', ' ', ' '.join([name, desc]))
+            parts = beer.select('text()').extract()
+            parts = map(lambda s: s.strip(), parts)
+            parts[0] = parts[0] + ":"
+            i['name'] = ' '.join(parts)
             items.append(i)
         items.sort(key=lambda e: e['name'])
         return items
